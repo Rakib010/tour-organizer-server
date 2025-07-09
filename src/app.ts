75ec4e1c69260869511fa31932091344c7bb71df
Rakib Hossain <hossainrokib55@gmail.com>
2025-07-09 11:14:15 +0600

    user  functionality

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,28 @@
 import express, { Request, Response } from "express"
-
+import cors from 'cors'
+import { router } from "./app/routes"
+import { globalErrorhandler } from "./app/middlewares/globalErrorHandler"
+import notFound from "./app/middlewares/notFound"
 
 const app = express()
 
+// middlewares
+app.use(express.json())
+app.use(cors())
+
+// Route
+app.use("/api/v1", router)
+
+// Root route 
 app.get("/", (req: Request, res: Response) => {
     res.status(200).json({
         message: "Welcome to Tour Management System Backend"
     })
-
 })
+
+// error middlewares
+app.use(globalErrorhandler)
+app.use(notFound)
+
 export default app
+
