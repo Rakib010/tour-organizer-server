d77fac38a2f7867f7efcc58ba5d385f9e612767f
Rakib Hossain <hossainrokib55@gmail.com>
2025-07-16 09:17:00 +0600

    Error validtion

---
+++ src/app/modules/tour/tour.interface.ts
@@ -0,0 +1,26 @@
+import { Types } from "mongoose";
+
+
+export interface ITourType {
+    name: string
+}
+
+
+export interface ITour {
+    title: string
+    slug: string
+    description?: string
+    images: string[]
+    location?: string
+    costForm?: number
+    starDate?: Date
+    endDate?: Date
+    included?: string[]
+    exclude?: string[]
+    amenities?: string[]
+    tourPlan?: string[]
+    maxGuest?: number
+    minAge?: number
+    division: Types.ObjectId
+    tourType: Types.ObjectId
+}
--- src/app/utils/catchAsync.ts
+++ src/app/utils/catchAsync.ts
@@ -1,7 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { NextFunction, Request, Response } from "express";
 
-
 type AsyncHandler = (req: Request, res: Response, next: NextFunction) => Promise<void>;
 
 export function catchAsync(fn: AsyncHandler) {
