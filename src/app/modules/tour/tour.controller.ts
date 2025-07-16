303529fb33cd2bcaf3b38c2d9297223146e5348e
Rakib Hossain <hossainrokib55@gmail.com>
2025-07-16 18:53:08 +0600

    tour crud

--- src/app/modules/tour/tour.controller.ts
+++ src/app/modules/tour/tour.controller.ts
@@ -5,20 +5,62 @@ import { tourServices } from "./tour.service"
 import { sendResponse } from "../../utils/sendResponse"
 
 const createTour = catchAsync(async (req: Request, res: Response) => {
-
-    const payload = req.body
-
-    const result = await tourServices.createTour(payload)
+    const result = await tourServices.createTour(req.body);
 
     sendResponse(res, {
         success: true,
         statusCode: httpStatus.CREATED,
-        message: "Division Created Successfully",
-        data: result
-    })
-})
+        message: 'Tour created successfully',
+        data: result,
+    });
+});
+
+/* const getAllTours = catchAsync(async (req: Request, res: Response) => {
+    //  const { page = 1, limit = 10, sortBy = 'createdAt', sortOrder = 'desc' } = req.query;
+
+    const result = await tourServices.getAllTours();
+
+    sendResponse(res, {
+        success: true,
+        statusCode: httpStatus.OK,
+        message: 'Tours retrieved successfully',
+        data: result,
+    });
+});
+ */
+const updateTour = catchAsync(async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const payload = req.body;
+
+    const result = await tourServices.updateTour(id, payload);
+
+    sendResponse(res, {
+        success: true,
+        statusCode: httpStatus.OK,
+        message: 'Tour updated successfully',
+        data: result,
+    });
+});
+
+const deleteTour = catchAsync(async (req: Request, res: Response) => {
+    const { id } = req.params;
+
+    const result = await tourServices.deleteTour(id);
+
+    sendResponse(res, {
+        success: true,
+        statusCode: httpStatus.OK,
+        message: 'Tour deleted successfully',
+        data: result,
+    });
+});
+
+
 
 
 export const tourController = {
-    createTour
+    createTour,
+    //getAllTours,
+    updateTour,
+    deleteTour
 }
--- src/app/modules/tour/tour.route.ts
+++ src/app/modules/tour/tour.route.ts
@@ -1,8 +1,22 @@
 import { Router } from "express";
+import { tourController } from "./tour.controller";
+import { checkAuth } from "../../middlewares/checkAuth";
+import { Role } from "../user/user.interface";
+import { createTourZodSchema } from "./tour.validation";
+import { validateRequest } from "../../middlewares/validateRequest";
 
 const router = Router()
 
-/* router.post('/create', tourController.createTour) */
+router.post(
+    '/create',
+    validateRequest(createTourZodSchema),
+    checkAuth(Role.ADMIN, Role.SUPER_ADMIN),
+    tourController.createTour
+);
+
+//router.get('/', tourController.getAllTours);
+router.patch('/:id', checkAuth(Role.ADMIN, Role.SUPER_ADMIN), tourController.updateTour);
+router.delete('/:id', checkAuth(Role.SUPER_ADMIN, Role.ADMIN), tourController.deleteTour);
 
 
 
